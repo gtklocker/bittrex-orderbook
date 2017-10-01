7c174d1e8f01348f3443b306fd01b5c7757ad94f
Kostis Karantias <karantiaskostis@gmail.com>
2017-10-01 20:50:44 +0300

    Get the initial orderbook before pulling diffs

--- index.js
+++ index.js
@@ -8,6 +8,28 @@ class BittrexOrderBook extends EventEmitter {
         return this.conn.call('SubscribeToExchangeDeltas', market);
     }
 
+    getInitialState(market) {
+        return this.conn
+            .call('QueryExchangeState', market)
+            .then(state => {
+                let { Sells, Buys } = state;
+
+                // type 0 means new order
+                const addTypeZero = order => {
+                    return {
+                        Type: 0,
+                        ...order
+                    };
+                };
+                Sells = Sells.map(addTypeZero);
+                Buys = Buys.map(addTypeZero);
+                this.onUpdateExchangeState({
+                    Sells,
+                    Buys
+                });
+            })
+    }
+
     onUpdateExchangeState(update) {
         update.Sells.forEach(this.askOrderBook.onOrderEvent);
         if (update.Sells.length > 0) {
@@ -22,14 +44,12 @@ class BittrexOrderBook extends EventEmitter {
 
     setupConn() {
         this.conn = new BittrexConnection;
-        this.conn.on('updateExchangeState', this.onUpdateExchangeState.bind(this));
+        this.conn.on('updateExchangeState', this.onUpdateExchangeState);
 
         this.conn
             .connect()
-            .then(() => {
-                this.subscribeToMarket(this.market);
-            });
-
+            .then(() => this.getInitialState(this.market))
+            .then(() => this.subscribeToMarket(this.market));
     }
 
     // TODO: allow subscription to multiple markets concurrently
@@ -41,6 +61,8 @@ class BittrexOrderBook extends EventEmitter {
         this.askOrderBook = new AskOrderBook();
         this.getTopAsk = this.askOrderBook.getTop;
         this.getTopBid = this.bidOrderBook.getTop;
+
+        this.onUpdateExchangeState = this.onUpdateExchangeState.bind(this);
 
         this.setupConn();
     }
